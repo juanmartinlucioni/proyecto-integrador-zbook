c98d4965ecca725e11c86e0dbcbabad4d8e40e3d
juanmartinlucioni <jlucioni@udesa.edu.ar>
2020-09-10 23:47:09 -0300

    conexcion post/login

--- zbook/app.js
+++ zbook/app.js
@@ -8,6 +8,8 @@ var logger = require('morgan');
 // Routers
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/login')
+var postRouter = require('./routes/post')
 
 var app = express();
 
@@ -24,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Llamada a Rutas
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/login', loginRouter);
+app.use('/post', postRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ zbook/controllers/postController.js
@@ -0,0 +1,7 @@
+let postController = {
+    index: function (req, res) {
+        return res.render("post", )
+    },
+};
+
+module.exports = postController;

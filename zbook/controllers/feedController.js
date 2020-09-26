c0da6066dc8d6fb59dd6d056e2fa9f1b0863fc8b
MatuAyerza <matu.ayerza@gmail.com>
2020-09-25 23:52:48 -0300

    css paginas ppales + vuelco json

--- zbook/controllers/feedController.js
+++ zbook/controllers/feedController.js
@@ -3,7 +3,12 @@ let listaPosts = require('../modules/postsModule');
 let listaComentarios = require('../modules/commentsModule');
 const controller = {
     index: (req, res) => {
-        res.render('feed', { title: 'Z-Book', listaUsuarios: listaUsuarios.lista, listaPosts: listaPosts.lista, listaComentarios: listaComentarios.lista});
+        res.render('feed', {
+            title: 'Z-Book',
+            listaUsuarios: listaUsuarios.lista,
+            listaPosts: listaPosts.lista,
+            listaComentarios: listaComentarios.lista
+        });
     },
 }
 

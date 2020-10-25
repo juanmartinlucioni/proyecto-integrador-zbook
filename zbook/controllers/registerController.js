b1ba329e7114bc6988dcfca3c621de2653fad119
MatuAyerza <matu.ayerza@gmail.com>
2020-10-24 22:26:46 -0300

    register start

--- zbook/controllers/registerController.js
+++ zbook/controllers/registerController.js
@@ -1,7 +1,22 @@
+const db = require('../config/database/models/Usuarios')
+const bcrypt = require('bcryptjs')
 const controller = {
     index: (req, res) => {
         res.render("register", { title: "Register" });
     },
+    add: (req, res) => {
+        let formData = req.body
+        let userPassword = bcrypt.hashSync(req.body.psw, 10)
+        newUser = {
+            username: formData.username,
+            email: formData.email,
+            password: userPassword,
+            age: formData.age,
+            birthday: formData.birthday
+        }
+        db.create(newUser)
+        res.redirect('/login')
+    }
 }
 
 module.exports = controller

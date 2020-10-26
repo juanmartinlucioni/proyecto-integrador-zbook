ed283e1430100752cece3be9947de7291f795807
jim-rattagan <jim.rattagan123@gmail.com>
2020-10-26 15:26:13 -0300

    post

--- zbook/routes/post.js
+++ zbook/routes/post.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
-let postController = require('../controllers/postController')
+let postController = require('../controllers/postController');
 
-router.get('/', postController.index)
+router.get('/', postController.index);
+router.get('/', postController.add);
+router.post('/', postController.store);
 
 module.exports = router;

ba083376df3a579ab21fbd09b2fdd018bf928bdc
jim-rattagan <jim.rattagan123@gmail.com>
2020-10-26 17:01:53 -0300

    rememberme checkbox

--- zbook/controllers/loginController.js
+++ zbook/controllers/loginController.js
@@ -1,5 +1,6 @@
 const db = require('../config/database/models/Usuarios')
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
+const { removeTicks } = require('sequelize/types/lib/utils');
 let loginController = {
   index: function (req, res) {
     return res.render("login", { title: "Login" });
@@ -20,6 +21,9 @@ let loginController = {
             if (bcrypt.compareSync(user.password, usuarioEncontrado.password)){
               console.log("bienvenido maquina!");
               req.session.user= usuarioEncontrado;
+              if(req.body.rememberme != undefined){
+                res.cookie('userId', user.id, {maxAge:20*1000})
+              };
             }
             else {
               console.log("alguien se olvido la contrasena");
@@ -34,6 +38,9 @@ let loginController = {
                   if (bcrypt.compareSync(user.password, mailEncontrado.password)){
                     console.log("que loco pa, te conectaste con el mail");
                     req.session.user= mailEncontrado;
+                    if(req.body.rememberme != undefined){
+                      res.cookie('userId', user.id, {maxAge:20*1000})
+                    };
                   }
                   else {
                     console.log("alguien se olvido la contrasena");
@@ -45,6 +52,9 @@ let loginController = {
             })
           }
         })
+        if(req.body.rememberme != undefined){
+          res.cookie('userId', user.id, {maxAge:20*1000})
+        }
     }
 };
 
